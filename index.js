3cbaf9cd1a70bfcaeea125977a9b858b463f24f7
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-03-06 15:20:31 +0100

    Updated question 5

--- index.js
+++ index.js
@@ -48,8 +48,13 @@ console.log("The maximum number in the array is " + max );
 /*
 5. Write a function called valTimesIndex which accepts an array of numbers and returns a new array with each value multiplied by the index it is at in the array: e.g., valTimesIndex([1,2,3]) // [0,2,6] valTimesIndex([5,10,15]) // [0,10,30]
  */
+function valTimesIndex(arrayOfnumbers) {
+  return arrayOfnumbers.map(function(value, index) {
+    return value * index;
+  });
+}
 
-const number = [1,2,3];//can be tested with any array set
-const filtered = number.filter(n => n <= 3);
-const items = filtered.map(n => n*(n-1));
-console.log(items);
+console.log(valTimesIndex([5,10,15])); 
+
+
+
